32c64148885b0c5c8d4aca719dc3bc2299985fe4
awesuh <bis16014@byui.edu>
2018-02-21 17:48:18 -0700

    MINE!!!!

--- app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
+++ app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String Owner = "Benjamin Earl";
+        String Owner = "CLARA BISHOP";
     }
 }
