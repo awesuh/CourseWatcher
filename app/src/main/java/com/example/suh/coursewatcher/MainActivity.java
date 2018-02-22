d636cb23d4286f4d98252e9a91c0c09bb7b1684b
Benjamin Earl <benearl72@gmail.com>
2018-02-21 17:55:00 -0700

    I give up

--- app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
+++ app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String Owner = "CLARA BISHOP";
-        String Co_Owners = "Ben and Jose";
+        String Owner = "CLARA BISHOP"; // 403
+        String Co_Owners = "Ben and Jose"; // 404
     }
 }
