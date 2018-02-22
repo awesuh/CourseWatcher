6b4e6cad8d053e49299e7f3aee45631eec52d0b4
joseareyejr <rey13012@byui.edu>
2018-02-21 17:55:20 -0700

    I win

--- app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
+++ app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
 
         String Owner = "CLARA BISHOP";
         String Co_Owners = "Ben and Jose";
+        String THE_Ownerjk = "Jose...";
     }
 }
