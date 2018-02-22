ccd8430ef7f890042831d105fd335c3eeb0b661c
Benjamin Earl <benearl72@gmail.com>
2018-02-21 17:50:05 -0700

    I give up

--- app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
+++ app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String Owner = "CLARA BISHOP";
+        String Owner = "CLARA BISHOP"; // Alright fine you boss
     }
 }
