eba735480ff634b47afbf6ba8416194c0fa9ec63
Benjamin Earl <benearl72@gmail.com>
2018-02-27 20:15:18 -0700

    Added some awesome tests!

---
+++ app/src/test/java/com/example/suh/coursewatcher/MyTest.java
@@ -0,0 +1,34 @@
+package com.example.suh.coursewatcher;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class MyTest {
+    @Test
+    public void can_get_section() throws Exception {
+        // uncomment after done stubbing your toe
+//        ApiWrapper testWrapper = new ApiWrapper();
+//        Section testSection = testWrapper.getSection("ACCTG201-01");
+//        assertEquals(testSection.code, "ACCTG201-01");
+    }
+    @Test
+    public void can_get_course() throws Exception {
+        // uncomment after done stubbing your toe
+//        ApiWrapper testWrapper = new ApiWrapper();
+//        Section testSection = testWrapper.getCourse("ACCTG201");
+//        assertEquals(testSection.code, "ACCTG201");
+    }
+    @Test
+    public void can_get_teacher() throws Exception {
+        // uncomment after done stubbing your toe
+//        ApiWrapper testWrapper = new ApiWrapper();
+//        Section testSection = testWrapper.getTeacher("Bro. Rodger");
+//        assertEquals(testSection.code, "Bro. Rodgers");
+    }
+}
