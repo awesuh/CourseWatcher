88ea32b5815685821ca5848648058010cd642dbc
awesuh <bis16014@byui.edu>
2018-03-03 10:12:47 -0700

    Basic UI stuff

--- app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
+++ app/src/main/java/com/example/suh/coursewatcher/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.suh.coursewatcher;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.search_results);
 
         String Owner = "CLARA BISHOP";
         String Co_Owners = "Ben and Jose";
---
+++ app/src/main/java/com/example/suh/coursewatcher/SectionListActivity.java
@@ -0,0 +1,17 @@
+package com.example.suh.coursewatcher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Suh on 3/3/2018.
+ */
+
+public class SectionListActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.section_list);
+    }
+
+}
