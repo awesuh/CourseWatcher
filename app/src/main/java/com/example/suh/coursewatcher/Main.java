2361cc972fa85e07c783b6ffe7e2f71db1ba8342
Benjamin Earl <benearl72@gmail.com>
2018-03-06 19:26:57 -0700

    did something, idk what

--- app/src/main/java/com/example/suh/coursewatcher/Main.java
+++ app/src/main/java/com/example/suh/coursewatcher/Main.java
@@ -1,5 +1,6 @@
 package com.example.suh.coursewatcher;
 
+import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,10 +9,16 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+
 public class Main extends AppCompatActivity {
+    SharedPreferences mPrefs  ;
+    SharedPreferences.Editor prefsEditor ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +32,23 @@ public class Main extends AppCompatActivity {
         String THE_Ownerjk = "Jose...";
 
         //Hi
+        mPrefs = getDefaultSharedPreferences(this);
+        prefsEditor = mPrefs.edit();
+
+        getObject();
+    }
+
+    public User getObject(){
+        Gson gson = new Gson();
+        String json = mPrefs.getString("user_settings", "");
+        User obj = gson.fromJson(json, User.class);
+        return obj;
+    }
+
+    public void saveObject(User user){
+        Gson gson = new Gson();
+        String json = gson.toJson(user);
+        prefsEditor.putString("user_settings", json);
+        prefsEditor.commit();
     }
 }
