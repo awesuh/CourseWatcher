e3233c6e1b9dbe96fa916d2147c3f328b4421bff
awesuh <bis16014@byui.edu>
2018-04-06 10:15:23 -0600

    Added getters and setters to Section to fix display bug.

--- app/src/main/java/com/example/suh/coursewatcher/Section.java
+++ app/src/main/java/com/example/suh/coursewatcher/Section.java
@@ -29,6 +29,94 @@ public class Section {
     public String session;
     public String status;
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public ArrayList<Instructor> getInstructors() {
+        return instructors;
+    }
+
+    public void setInstructors(ArrayList<Instructor> instructors) {
+        this.instructors = instructors;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Schedule> getSchedules() {
+        return schedules;
+    }
+
+    public void setSchedules(ArrayList<Schedule> schedules) {
+        this.schedules = schedules;
+    }
+
+    public int getSeatsFilled() {
+        return seatsFilled;
+    }
+
+    public void setSeatsFilled(int seatsFilled) {
+        this.seatsFilled = seatsFilled;
+    }
+
+    public int getSeatsTotal() {
+        return seatsTotal;
+    }
+
+    public void setSeatsTotal(int seatsTotal) {
+        this.seatsTotal = seatsTotal;
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    public void setSection(int section) {
+        this.section = section;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     public Section(){
 //        Log.d(TAG,"Creating a section");
     }
